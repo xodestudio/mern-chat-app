22cfd6381aa9984a907f4ba09b044f1eb2b8b086
Xode Studio <alidevofficial@gmail.com>
2025-01-07 23:50:17 +0500

    add user model and update dependencies

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
