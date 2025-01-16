6f14f0c94192fa66ffeefc84cd47151bc9313b98
Xode Studio <alidevofficial@gmail.com>
2025-01-16 16:01:38 +0500

    add routing with Home, Signup, and Login components; configure Prettier settings and update package dependencies

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./components/Home.jsx"
 import './index.css'
 import App from './App.jsx'
+import Signup from './components/Signup.jsx'
+import Login from './components/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>,
 )
